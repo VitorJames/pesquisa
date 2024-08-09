162f39e43e8e02395d40210a633c7a8c07d7fb52
Vitor James <vitorjames2@gmail.com>
2024-08-09 08:45:37 -0300

    Teste pwa 4

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { registerServiceWorker } from '../serviceWorker.ts'
-registerServiceWorker();
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.unregister();
+
+    if (caches) {
+      // Service worker cache should be cleared with caches.delete()
+      caches.keys().then(async (names) => {
+        await Promise.all(names.map(name => caches.delete(name)));
+      });
+    }
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
