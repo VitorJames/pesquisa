f8f4b738c1f6deae1227d0b2b869b1e8ab27ca12
Vitor James <vitorjames2@gmail.com>
2024-08-09 09:02:30 -0300

    sem pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.ready.then(registration => {
-    registration.unregister();
-
-    if (caches) {
-      // Service worker cache should be cleared with caches.delete()
-      caches.keys().then(async (names) => {
-        await Promise.all(names.map(name => caches.delete(name)));
-      });
-    }
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
